f537e37399aee5b49c1f0ea63a4f3fde45dd6430
SorenPatel10 <m284932@usna.edu>
2026-02-16 20:46:56 -0500

    lab06 done

--- lab06/DumpTweetQueue.java
+++ lab06/DumpTweetQueue.java
@@ -1,17 +1,38 @@
+/*
+Soren Patel, Sid Swarup
+Lab06
+DumpTweetQueue.java
+*/
+
 import java.util.*;
 import si211.*;
 import java.io.*;
 
+/**
+ * DumpTweetQueue class
+ * extends TweetQueue with dump functionality
+ */
 public class DumpTweetQueue extends TweetQueue{
+    
+    //Iterator field
     protected TweetQueue.Iter tweetIter;
 
+    /**
+     * Empty constructor, intiate with super
+     * initiate iterator
+     */
     public DumpTweetQueue(){
         super();
         tweetIter = this.iterator();
     }
 
+    /**
+     * dump method
+     * prints all tweets in the current DumpTweetQueue
+     */
     public void dump(){
         TweetQueue.Iter tweetIter = this.iterator();
+        //loop until end of queue
         while(tweetIter.hasNext()){
             System.out.println(tweetIter.next());
         }
--- lab06/FilterTweetQueue.java
+++ lab06/FilterTweetQueue.java
@@ -1,49 +1,85 @@
+/*
+Soren Patel, Sid Swarup
+Lab06
+FilterTweetQueue.java
+*/
+
 import java.util.*;
 import si211.*;
 import java.io.*;
 
+/**
+ * FilterTweetQueue class
+ * extends DumpTweetQueue with filtering functionality
+ */
 public class FilterTweetQueue extends DumpTweetQueue {
+    
+    //count field keep track of how many elements in queue
     private int count;
 
+    /**
+     * Constructor
+     */
     public FilterTweetQueue() {
         super();
         this.count = 0;
     }
 
+    /**
+     * Override enqueue from Queue
+     */
     public void enqueue(Tweet t) {
+        //increment count
         count++;
         super.enqueue(t);
     }
 
+    /**
+     * Override dequeue from Queue
+     */
     public Tweet dequeue(Tweet t) {
+        //decrement count
         count--;
         return super.dequeue();
     }
 
-
-
+    /**
+     * filter method
+     * returns FilterTweetQueue with only tweets containing
+     * the String specified in the parameter
+     */
     public FilterTweetQueue filter(String word) {
         FilterTweetQueue filtered = new FilterTweetQueue();
 
+        //initiate iterator
         TweetQueue.Iter tweetIter = this.iterator();
 
+        //use indexOf() to check if meets filter requiremetns
         while(tweetIter.hasNext()){
             Tweet temp = tweetIter.next();
             if(temp.getText().indexOf(word) >= 0) {
                 filtered.enqueue(temp);
             }
         }
+        //report how many were kept
         System.out.println(filtered.count + " tweets");
         return filtered;
     }
 
+    /**
+     * filterBand method
+     * returns FilterTweetQueue with only tweets 
+     * not containing the parameter String
+     */
     public FilterTweetQueue filterBang(String word) {
         FilterTweetQueue filterBanged = new FilterTweetQueue();
 
+        //initiate iterator
         TweetQueue.Iter tweetIter = this.iterator();
 
         while(tweetIter.hasNext()){
             Tweet temp = tweetIter.next();
+                //indexOf returns -1, means no string found
                 if(temp.getText().indexOf(word) < 0) {
                     filterBanged.enqueue(temp);
                 }
@@ -52,8 +88,9 @@ public class FilterTweetQueue extends DumpTweetQueue {
         return filterBanged;
     }
 
-
-
+    /**
+     * getter method for count field
+     */
     public int getCount() {
         return count;
     }
