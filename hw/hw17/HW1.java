2c6709b40e53bacd920b6919f722126d5680f1e7
SorenPatel10 <m284932@usna.edu>
2026-03-24 19:11:47 -0400

    hw17

--- hw/hw17/HW1.java
+++ hw/hw17/HW1.java
@@ -5,14 +5,34 @@ public class HW1
 {
   public static void main(String[] args)
   {
-    Reader r = new InputStreamReader(System.in);
-    Scanner sc = new Scanner(r);
-    try {
-      System.out.println(Mystery.compute(sc));
+    
+    LineNumberReader reader = null;
+    try{
+      if(args.length > 0){
+        try{
+          reader = new LineNumberReader(new FileReader(args[0]));
+        }
+        catch(FileNotFoundException f){
+          System.out.println("File \""+ args[0] + "\" not found");
+          return;
+        }
+      }
+      else
+        reader = new LineNumberReader(new InputStreamReader(System.in));
+
+      Scanner sc = new Scanner(reader);
+      try {
+        System.out.println(Mystery.compute(sc));
+      }
+      catch(Exception e)
+      {
+        System.out.println("Error " + e.getMessage() + " at line " + (reader.getLineNumber()));
+      }
     }
-    catch(Exception e)
-    {
-      System.out.println("Error " + e.getMessage());
-    }
+  catch(Exception e){
+    System.out.println("Error " + e.getMessage());
+  }
+
+    
   }
 }
