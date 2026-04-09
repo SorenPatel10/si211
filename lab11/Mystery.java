98c902e16e421513b89d0655198e558aa75a3c1c
SorenPatel10 <m284932@usna.edu>
2026-04-09 15:20:09 -0400

    sdfs

---
+++ lab11/Mystery.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Mystery implements ActionListener
+{
+  private JLabel label;
+  public Mystery(JLabel label) { this.label = label; }
+  public void actionPerformed(ActionEvent e)
+  {
+    CChange.changeColor(label);
+  }
+}
